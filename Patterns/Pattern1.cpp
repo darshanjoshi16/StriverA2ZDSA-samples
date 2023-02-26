bfa4461305fc4ca5a7b8a95207a024514f5ae4a3
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2023-02-26 12:11:28 +0530

    Pattern1.cpp creation

---
+++ Patterns/Pattern1.cpp
@@ -0,0 +1,27 @@
+/* Pattern 1
+Input: 5
+
+Output:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
+
+
+void printSquare(int n) {
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                cout << '*'<<" ";
+            }
+            
+            cout << endl;
+        }
+    }
+};
+
