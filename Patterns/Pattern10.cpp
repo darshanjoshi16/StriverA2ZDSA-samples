1762e13ac3a34b3bc0ba2c68fa01091849e9305b
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2023-02-26 12:33:48 +0530

    pattern10.cpp creation

---
+++ Patterns/Pattern10.cpp
@@ -0,0 +1,49 @@
+/*
+Input: 5
+
+Output:
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+void printTriangle(int n) 
+    {
+        for(int i=1;i<=2*n-1;i++)
+        {
+            //stars printing
+            
+            int stars = i;
+            if(i>n)
+            {
+                stars=2*n-i;
+            }
+            
+            for(int j=0;j<stars;j++)
+            {
+                cout << "* ";
+            }
+            
+            //spaces printing
+            int spaces = n-1;
+            for(int j=0;j<spaces;j++)
+            {
+                cout << " ";
+            }
+            if(i>n)
+            {
+                spaces = spaces + 2;
+            }
+            else {spaces= spaces- 2;}
+            cout << endl;
+        }
+        
+    }
+};
