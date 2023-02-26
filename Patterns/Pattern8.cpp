85820f4c538ccc2b8940341d27b9429f51033711
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2023-02-26 12:26:30 +0530

    pattern8.cpp creation

---
+++ Patterns/Pattern8.cpp
@@ -0,0 +1,40 @@
+/*
+Input: 5
+
+Output:
+
+*********
+ *******
+  *****
+   ***
+    *
+    
+*/
+
+void printTriangle(int n) {
+	     
+	     for(int i=0; i<n; i++)
+        {
+            //space printing
+            for(int j=0;j<i;j++)
+            {
+                cout << " ";
+            }
+            
+            
+            //stars printing
+            for(int j=0; j<2*n-(2*i+1);j++)
+            {
+                cout << "*";
+            }
+            
+            //space printing
+            for(int j=0;j<i;j++)
+            {
+                cout << " ";
+            }
+            cout << endl;
+            
+        }
+	}
+};
