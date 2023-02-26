7c943df489d037a5194c4e45e81de2fe7517ad5e
Darshan Joshi <70624714+darshanjoshi16@users.noreply.github.com>
2023-02-26 12:31:09 +0530

    pattern9.cpp creation

---
+++ Patterns/Pattern9.cpp
@@ -0,0 +1,44 @@
+/*
+Input : 5
+
+Output:
+
+     *
+    ***
+   *****
+  *******
+ *********
+ *********
+  *******
+   *****
+    ***
+     *
+     
+ */
+
+   void printDiamond(int n) 
+    {
+       for(int r=1 ; r<= 2*n ; r++  ) 
+       {
+
+         //used ternary operator to determine the row based on condition of the symmetry
+        int row = r <= n ? r : 2*n - r+1;
+
+            for(int s=1 ; s <= n - row ; s++) 
+            {
+
+                cout<<" ";
+
+            }
+
+            for(int c=1 ; c <= row ; c++) 
+            {
+
+                cout<<"* ";
+
+            }
+
+            cout<<endl;
+        }
+    }
+};
